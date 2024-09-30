9101f417e2bfc5de40b387b109255e530711d0ee
kobihall <kobihallacc@gmail.com>
2024-09-30 21:53:23 +0200

    More engine shell

--- src/vk_types.h
+++ src/vk_types.h
@@ -1,7 +1,32 @@
 #pragma once
 
-//#define GLFW_INCLUDE_NONE
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+#include <span>
+#include <array>
+#include <functional>
+#include <deque>
+
+#define GLFW_INCLUDE_NONE
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 #include <vulkan/vulkan.h>
+
+#include <VulkanMemoryAllocator/include/vk_mem_alloc.h>
+
+#include <fmt/core.h>
+
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+
+#define checkVkResult(x)                                        \
+do{                                                             \
+    VkResult err = x;                                           \
+	if (err) {                                                  \
+        fmt::println("Detected Vulkan error: {}", string_VkResult(err)); \
+        abort();                                                \
+    }                                                           \
+} while(0)
